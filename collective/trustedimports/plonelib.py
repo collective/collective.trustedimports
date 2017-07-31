a97f280c2a3e90cf319ff5fc92cd400e6f5819b1
Ivan Teoh <ivan.teoh@gmail.com>
2017-07-31 16:04:58 +1000

    Whitelist NamedBlobFile.Data, datastream.getData, RichTextValue.output

--- collective/trustedimports/plonelib.py
+++ collective/trustedimports/plonelib.py
@@ -36,3 +36,55 @@ persistent.list.PersistentList.__Security_checker__ = list_checker
 
 #TODO: move into taskqueue
 ModuleSecurityInfo("collective.taskqueue.taskqueue").declarePublic("add")
+
+# RichTextValue
+try:
+    import plone.app.textfield.value
+except ImportError:
+    plone.app.textfield.value = None
+
+if plone.app.textfield.value is not None:
+    plone.app.textfield.value.RichTextValue._security = sec_RichTextValue = ClassSecurityInfo()
+    sec_RichTextValue.declareObjectPublic()
+    sec_RichTextValue.declarePrivate('raw')
+    sec_RichTextValue.declarePrivate('encoding')
+    sec_RichTextValue.declarePrivate('raw_encoded')
+    sec_RichTextValue.declarePrivate('mimeType')
+    sec_RichTextValue.declarePrivate('outputMimeType')
+    sec_RichTextValue.declarePrivate('output_relative_to')
+    sec_RichTextValue.setDefaultAccess(1)
+    sec_RichTextValue.apply(plone.app.textfield.value.RichTextValue)
+    InitializeClass(plone.app.textfield.value.RichTextValue)
+
+# datastream
+try:
+    import Products.PortalTransforms.data
+except ImportError:
+    Products.PortalTransforms.data = None
+
+if Products.PortalTransforms.data is not None:
+    Products.PortalTransforms.data.datastream._security = sec_datastream = ClassSecurityInfo()
+    sec_datastream.declareObjectPublic()
+    sec_datastream.declarePrivate('setData')
+    sec_datastream.declarePrivate('setSubObjects')
+    sec_datastream.declarePrivate('getSubObjects')
+    sec_datastream.declarePrivate('setCacheable')
+    sec_datastream.setDefaultAccess(1)
+    sec_datastream.apply(Products.PortalTransforms.data.datastream)
+    InitializeClass(Products.PortalTransforms.data.datastream)
+
+# NamedBlobFile
+try:
+    import plone.namedfile.file
+except ImportError:
+    plone.namedfile.file = None
+
+if plone.namedfile.file is not None:
+    plone.namedfile.file.NamedBlobFile._security = sec_NamedBlobFile = ClassSecurityInfo()
+    sec_NamedBlobFile.declareObjectPublic()
+    sec_NamedBlobFile.declarePrivate('open')
+    sec_NamedBlobFile.declarePrivate('openDetached')
+    sec_NamedBlobFile.declarePrivate('_setData')
+    sec_NamedBlobFile.setDefaultAccess(1)
+    sec_NamedBlobFile.apply(plone.namedfile.file.NamedBlobFile)
+    InitializeClass(plone.namedfile.file.NamedBlobFile)
