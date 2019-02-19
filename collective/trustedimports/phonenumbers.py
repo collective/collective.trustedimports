c3511c55b30f04d5ee7b33616c5d5cdf0ee89024
Dylan Jay <software@pretaweb.com>
2019-02-19 17:50:16 +0700

    fix configure.zcml syntax

--- collective/trustedimports/phonenumbers.py
+++ collective/trustedimports/phonenumbers.py
@@ -1,7 +1,3 @@
-from AccessControl import ModuleSecurityInfo
-from Products.PythonScripts.Utility import allow_module
 from collective.trustedimports.util import whitelist_module
 
-import phonenumbers
-ModuleSecurityInfo('phonenumbers').declarePublic('render')
-whitelist_module('phonenumbers', [], ['country_code_for_region'])
+whitelist_module('phonenumbers')
