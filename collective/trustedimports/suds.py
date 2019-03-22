332be3801de418c95c50c6df528fa9295d92c590
quang.nguyen <quang@pretagov.com>
2019-03-22 16:38:22 +1100

    Whitelist suds client

--- collective/trustedimports/suds.py
+++ collective/trustedimports/suds.py
@@ -1,4 +1,33 @@
 from collective.trustedimports.util import whitelist_module
+import os
+from AccessControl import allow_class, ModuleSecurityInfo, ClassSecurityInfo, Unauthorized
+from AccessControl.class_init import InitializeClass
+from Products.PythonScripts.Utility import allow_module
+from suds.client import Client as _Client
+from suds import WebFault
+from suds import MethodNotFound
 
-whitelist_module('suds')
-whitelist_module('suds.client')
+
+class SafeClient(_Client):
+    """ SafeClient
+    """
+
+    def __init__(self, url, **kwargs):
+
+        blacklist = os.getenv('SAFEIMPORTS_URL_BLACKLIST ', '')
+        if url in blacklist:
+            raise Unauthorized("Not supported by SafeClient in this context")
+
+        return _Client.__init__(self, url, kwargs)
+
+
+# monkey patching zipfile
+suds.client.Client = SafeClient
+
+ModuleSecurityInfo('collective.trustedimports.suds').declarePublic('SafeClient')
+
+allow_class(SafeClient)
+ModuleSecurityInfo('suds').declarePublic('WebFault','MethodNotFound')
+
+allow_class(WebFault)
+allow_class(MethodNotFound)
