3e715641f3228407e362053549c7d4f0abdf0476
Dylan Jay <software@pretaweb.com>
2019-03-22 22:20:21 +0700

    fix

--- collective/trustedimports/suds.py
+++ collective/trustedimports/suds.py
@@ -3,17 +3,17 @@ import os
 from AccessControl import allow_class, ModuleSecurityInfo, ClassSecurityInfo, Unauthorized
 from AccessControl.class_init import InitializeClass
 from Products.PythonScripts.Utility import allow_module
-from suds.client import Client as _Client
+from suds.client import Client
 from suds import WebFault
 from suds import MethodNotFound
 
 
 # monkey patching zipfile
-wrap_protected(_Client.__init__, is_url_allowed)
+wrap_protected(Client.__init__, is_url_allowed)
 
 ModuleSecurityInfo('collective.trustedimports.suds').declarePublic('SafeClient')
 
-allow_class(SafeClient)
+allow_class(Client)
 ModuleSecurityInfo('suds').declarePublic('WebFault','MethodNotFound')
 
 allow_class(WebFault)
