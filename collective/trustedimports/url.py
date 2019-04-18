8d73bbb57d88f77dcad2fdf1d540b3ae450ec57c
quang.nguyen <quang@pretagov.com>
2019-04-18 21:36:56 +1000

    White urlisb and urlparse module

---
+++ collective/trustedimports/url.py
@@ -0,0 +1,9 @@
+from collective.trustedimports.util import whitelist_module, wrap_protected, is_url_allowed
+from AccessControl import allow_class, ModuleSecurityInfo, ClassSecurityInfo, Unauthorized
+from AccessControl.class_init import InitializeClass
+from Products.PythonScripts.Utility import allow_module
+import urllib
+import urlparse
+
+ModuleSecurityInfo('urllib').declarePublic('quote')
+ModuleSecurityInfo('urlparse').declarePublic('urlparse')
